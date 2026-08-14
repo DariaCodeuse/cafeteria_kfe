75d33f78c75a12fdafd28f1cd478d39dccc35797
Karla D <diazaguilarkarla8@gmail.com>
2026-08-14 01:19:56 -0600

    feat: add shadcn components and utilities: button and card

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
             </code>{" "}
             file.
           </h1>
+          <h2>Alcance la pàgina lol</h2>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
